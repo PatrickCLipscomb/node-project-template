56799abd15f246722935509b3775ee2418e7372e
Maarja Laineste and Patrick Lipscomb <patrick@>
2016-08-01 14:58:28 -0700

    finish the memory game app.

--- js/card.js
+++ js/card.js
@@ -16,3 +16,22 @@ Card.prototype.randomOrder = function() {
 
 
 exports.cardModule = Card;
+
+// 
+//
+// if ((clickCounter % 2 === 0) && (previousImage === imgName)) {
+//   $('#' + imgNum.toString()).html('<div id="' + imgNum.toString() + '" class="card"><img src="img/' + imgName + '.jpg" alt="' + imgNum + '" /></div>');
+//   clickCounter += 1;
+// } else if ((clickCounter % 2 === 0) && (previousImage !== imgName)) {
+//   $('#' + imgNum.toString()).html('<div id="' + imgNum.toString() + '" class="card"><img src="img/' + imgName + '.jpg" alt="' + imgNum + '" /></div>');
+//     if ((imgNum.charAt(imgNum.length) === 0)) {
+//       var finder = 1;
+//       var newImgNum = imgName + finder.toString();
+//         $('#' + imgName + finder.toString()).html('<div id="' + newImgNum + '" class="card"><img src="img/' + imgName + '.jpg" alt="' + newImgNum + '" /></div>');
+//     } else {
+//       var finder = 0;
+//       var newImgNum = previousImage + finder.toString();
+//         $('#' + imgName + finder.toString()).html('<div id="' + newImgNum + '" class="card"><img src="img/card-back.jpg" alt="' + newImgNum + '" /></div>');
+//     }
+//   clickCounter += 1;
+// } else {
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,13 +1,33 @@
 var Card = require('./../js/card.js').cardModule;
 
+var clickCounter = 1;
+var previousImage = null;
+
 $(document).ready(function() {
-  $('#play-the-game').on('click', function(event) {
+  $('#play-the-game').on('click', function() {
     var dummyCard = new Card('random group');
     var randomImages = [];
     for (var i = 0; i < 2; i++) {
       randomImages = dummyCard.randomOrder();
       randomImages.forEach(function(imgName) {
-        $('#cards').append('<li><div id="' + imgName + i + '" class="card"><img src="img/card-back.jpg" alt="' + imgName + '" /></div></li>');
+        var newImgNum = '';
+        var imgNum = imgName + i;
+        $('#cards').append('<li><div id="' + imgNum.toString() + '" class="card"><img src="img/card-back.jpg" alt="' + imgNum + '" /></div></li>');
+        $('#' + imgNum.toString()).on('click', function() {
+          if ((clickCounter % 2 === 0) && (previousImage.slice(0, previousImage.length - 1) === imgName)) {
+            $('#' + imgNum.toString()).html('<div id="' + imgNum.toString() + '" class="card"><img src="img/' + imgName + '.jpg" alt="' + imgNum + '" /></div>');
+            clickCounter += 1;
+          } else if ((clickCounter % 2 === 0) && (previousImage.slice(0, previousImage.length - 1) !== imgName)) {
+            $('#' + imgNum.toString()).html('<div id="' + imgNum.toString() + '" class="card"><img src="img/' + imgName + '.jpg" alt="' + imgNum + '" /></div>');
+            setTimeout(function(){ $('#' + imgNum.toString()).html('<div id="' + imgNum.toString() + '" class="card"><img src="img/card-back.jpg" alt="' + imgNum + '" /></div>'); }, 1000);
+            setTimeout(function(){ $('#' + previousImage).html('<div id="' + newImgNum + '" class="card"><img src="img/card-back.jpg" alt="' + newImgNum + '" /></div>'); }, 1000);
+            clickCounter += 1;
+          } else {
+            $('#' + imgNum.toString()).html('<div id="' + imgNum.toString() + '" class="card"><img src="img/' + imgName + '.jpg" alt="' + imgNum + '" /></div>');
+            clickCounter += 1;
+            previousImage = imgNum;
+          }
+        });
       });
     }
   });
